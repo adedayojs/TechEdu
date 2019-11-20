6f18a13da17823ae0f032f0d2b4d1c0e1fb83622
adedayojs <samfeolu@gmail.com>
2019-11-20 01:51:05 +0100

    Fix: Editied User Model

--- apis/model/user.ts
+++ apis/model/user.ts
@@ -1,6 +1,33 @@
-import { model } from 'mongoose';
-import user  from '../schema/user';
+import { model, Schema, Document } from 'mongoose';
 
-const Users = model('Users', user);
+interface IUser extends Document {
+  username: string;
+  firstname: string;
+  lastname: string;
+  phone: string;
+  email: string;
+  password: string;
+  gender: string;
+  isAdmin: boolean;
+  isLecturer: boolean;
+  isStudent: boolean;
+  institutions: IInstitution[]
+}
+const userSchema = new Schema({
+  username: { type: String, required: true },
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  phone: { type: String, required: false },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  gender: String,
+  isAdmin: { required: true, default: false, type: Boolean },
+  isLecturer: { required: true, default: false, type: Boolean },
+  isStudent: { required: true, default: false, type: Boolean },
+  institutions:[{
+      type:Schema.Types.ObjectId,
+      ref:'Institution'
+  }]
+});
 
-export default Users;
+export default model<IUser>('User', userSchema);
